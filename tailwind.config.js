35c02f971d767426027bf52322f7c5827917830f
fahadArnob-10 <fahadarnob67677@gmail.com>
2024-09-02 21:36:33 +0600

    wabsite modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*'],
+  content: ["*"],
   theme: {
+    fontFamily: {
+      nunito: "Nunito, sans-serif",
+      syne: "Syne, sans-serif",
+      poppins: "Poppins, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
